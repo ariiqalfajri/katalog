96b8edd7f92d286067d56e0ffbb267ec7deb072f
ariiqalfajri <ariiq.i.alfajri@gmail.com>
2025-04-25 08:11:28 +0700

    config db

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -30,6 +30,16 @@ return [
     */
 
     'connections' => [
+
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST','127.0.0.1'),
+            'port' => env('DB_PORT','27017'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'options' => ['database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),]
+        ],
 
         'sqlite' => [
             'driver' => 'sqlite',
